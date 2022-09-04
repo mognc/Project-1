9b48eb3b364f766803bc197fb3673effe0ae6f74
ali ahmad <76902280+mognc@users.noreply.github.com>
2022-09-05 03:32:13 +0500

    Add files via upload

---
+++ j1.js
@@ -0,0 +1,12 @@
+
+
+function floors(){
+    var floor = document.getElementById("floor_no").value;
+    alert("floor are: " + floor)
+
+}
+
+function rooms(){
+    var room = document.getElementById("room_no").value;
+    alert("rooms are: "  + room)
+}
